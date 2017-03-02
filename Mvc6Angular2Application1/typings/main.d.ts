ec0ab4f3584c20d0a0a1081ad653c32b1c127524
usman.ali <usman.ali@ZIN-44>
2017-03-02 17:39:11 +0500

    Deleting irrelevent folders

--- Mvc6Angular2Application1/typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/bootstrap/index.d.ts" />
-/// <reference path="main/ambient/core-js/index.d.ts" />
-/// <reference path="main/ambient/jquery/index.d.ts" />
--- ng.Net1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ng.Net1.WebApiApplication" Language="C#" %>
